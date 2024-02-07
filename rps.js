4041486ca1953ba0ec115bef54ee52ec2981b0f2
marysanders <lyremary@yahoo.com>
2024-02-07 17:08:47 -0600

    Add playGame() function

--- rps.js
+++ rps.js
@@ -1,7 +1,7 @@
-let randomNum= Math.floor(Math.random() * (3 - 0) ) + 0;
-console.log(randomNum);
+function getComputerChoice() {
 
-function getComputerChoice(randomNum) {
+let randomNum = Math.floor(Math.random() * (3 - 0) ) + 0;
+ 
     // Randomly return rock, paper, or scissors
     // return random number 
     // if statement 0 = rock, 1 = paper, 2 = scissors
@@ -16,13 +16,14 @@ if (randomNum == 0) {
 
 }
 
-console.log(getComputerChoice(randomNum));
+//  console.log(getComputerChoice(randomNum));
 
 // function plays single round
 // takes two parameters playerSelection, computerSelection
 // return string "You Lose! Paper beats Rock"
 // playerSelection param is case insenstive 
 // return result 
+
 
 function playRound(playerSelection, computerSelection) {
 
@@ -34,25 +35,48 @@ if (lowerCasePlayerSelection== "rock" && computerSelection == "Rock") {
         return "You lose! Paper beats rock!"
     } else if (lowerCasePlayerSelection == "rock" && computerSelection == "Scissors") {
         return "You win! Rock beats scissors!"
-    }
+    } 
 
-    if (lowerCasePlayerSelection== "paper" && computerSelection == "Paper") {
+ if (lowerCasePlayerSelection== "paper" && computerSelection == "Paper") {
         return "Tie!";
     } else if (lowerCasePlayerSelection == "paper" && computerSelection == "Rock") {
         return "You win! Paper beats rock!"
     } else if (lowerCasePlayerSelection == "paper" && computerSelection == "Scissors") {
         return "You lose! Scissors beats paper!"
-    }
+    } 
 
-    if (lowerCasePlayerSelection== "scissors" && computerSelection == "Scissors") {
+ if (lowerCasePlayerSelection== "scissors" && computerSelection == "Scissors") {
         return "Tie!";
     } else if (lowerCasePlayerSelection == "scissors" && computerSelection == "Paper") {
         return "You win! Scissors beats paper!"
     } else if (lowerCasePlayerSelection == "scissors" && computerSelection == "Rock") {
         return "You win! Scissors beats rock!"
+    } else {
+        return "Please enter rock, paper or scissors"
     }
+
 }
 
-const playerSelection = "Rock";
-const computerSelection = getComputerChoice(randomNum);
-console.log(playRound(playerSelection, computerSelection));
+// playGame()
+// use playRound() inside of playGame()
+// loop to call playRound() 5 times in a row 
+// use console.log() to display results of each round
+// use prompt() to get input from user 
+
+// function playGame() {
+//  playRound(playerSelection, computerSelection);
+// }
+
+function playGame() {
+
+    for (let i = 1; i <= 5; i++) {
+        let playerSelection = window.prompt("Please enter rock, paper, or scissors");
+        const computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+        console.log(playRound(playerSelection, computerSelection))
+        console.log(computerSelection);
+    }
+
+}
+
+playGame();
